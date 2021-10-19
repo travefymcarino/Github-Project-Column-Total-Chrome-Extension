3093f28203cdf08c0ef174074873c70e24c1cf03
Nathan Clark <nathan@bernadene.local>
2021-10-19 10:35:27 -0500

    First

---
+++ js/inject.js
@@ -0,0 +1,29 @@
+(function() {
+  var desiredIndex = null;
+
+  var columnheaders = document.querySelectorAll('[role=columnheader]');
+  for (let i = 0; i < columnheaders.length; i++) {
+    if (columnheaders[i].firstElementChild.firstChild.innerText == 'Estimate (hours)') {
+      desiredIndex = i;
+    }
+  }
+
+  var estimateTotal = 0;
+  var rows = document.querySelectorAll('[role=row]');
+  for (let i = 0; i < rows.length; i++) {
+    if (rows[i].children[desiredIndex] && rows[i].children[desiredIndex].firstElementChild && rows[i].children[desiredIndex].firstElementChild.firstChild) {
+      var thing = parseFloat(rows[i].children[desiredIndex].firstElementChild.firstChild.innerText);
+      if (!isNaN(thing)) {
+        estimateTotal = estimateTotal + thing;
+      }
+    }
+  }
+  alert(estimateTotal);
+  // var aChild = document.createElement("p");
+  // aChild.classList.add("fiDMsB");
+  // aChild.style.marginLeft = '3px';
+  // var total = document.createTextNode(estimateTotal);
+  // aChild.appendChild(total);
+  // columnheaders[desiredIndex].firstElementChild.firstChild.appendChild(aChild);
+})();
+
